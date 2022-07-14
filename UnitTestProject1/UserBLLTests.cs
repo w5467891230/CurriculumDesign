9a131a899d9fd01eab7fc2e82237f08fbf903b3b
Branch <liuke745@163.com>
2022-07-14 23:41:21 +0800

    添加测试

---
+++ UnitTestProject1/UserBLLTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Tests
+{
+    [TestClass()]
+    public class UserBLLTests
+    {
+        [TestMethod()]
+        public void AddUserMethodTest()
+        {
+            Model.manager user = new Model.manager();
+            user.MName = "55";
+            user.MCode = "55";
+            user.MCodeRe = "55";
+            user.RadioManage = true;
+            user.RadioWork = false;
+
+            BLL.UserBLL userBLL = new BLL.UserBLL();
+            string result = userBLL.AddUserMethod(user);
+            Assert.AreEqual("添加成功", result);
+        }
+    }
+}
